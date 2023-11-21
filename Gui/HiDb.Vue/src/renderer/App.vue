63988d1327ee41b231c7c32596cb104204c4c5c9
zhaoqiangqiang <zhaoqiangqiang@benlai.com>
2023-11-21 10:18:15 +0800

    init hidb vue

---
+++ Gui/HiDb.Vue/src/renderer/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <title-bar />
+  <router-view v-slot="{ Component }">
+    <component :is="Component" />
+  </router-view>
+</template>
+
+<script setup lang="ts">
+import TitleBar from "./components/common/TitleBar.vue";
+
+</script>
+
+<style></style>
